e422c1f328f73c2ac95f27e8f75d87fe2ad97462
Gopinath (aka) bluepie <gopinath2nr@gmail.com>
2021-07-06 03:35:07 +0530

    add tests for jwt token

--- test/api.test.ts
+++ test/api.test.ts
@@ -1,3 +1,5 @@
+import { assert } from 'console';
+
 import request from 'supertest';
 
 import app from '../src/app';
@@ -20,27 +22,119 @@ describe('GET /top/askdf', () => {
   });
 });
 
-describe('GET /top', () => {
-  it('should return 200 OK', (done) => {
-    request(app).get('/top').expect('Content-Type', /json/).expect(200, done);
+describe('GET /top, accessing without token', () => {
+  it('should return 401 Unauthorized', (done) => {
+    request(app).get('/top').expect('Content-Type', 'text/html; charset=utf-8').expect(401, done);
+  });
+});
+
+const testUser = {
+  id: 'testCaseUser',
+  uniqueHash:
+    '382cd78f2fcd04561940af9d9826c353fe75c80433287e757dc75c3d80b629caa6558' +
+    '7496317d3de7942721f71e85ea3bfa24e21543e6101c445c989254cdbd4',
+};
+
+describe('POST /authenticate, create token', () => {
+  it('should return 400 with empty user', (done) => {
+    request(app).post('/authenticate').set('Content-Type', 'application/json').send({}).expect(400, done);
+  });
+
+  it('should return 400 with invalid id', (done) => {
+    request(app)
+      .post('/authenticate')
+      .set('Content-Type', 'application/json')
+      .send({ id: testUser.id + 'invalid', uniqueHash: testUser.uniqueHash })
+      .expect(400, done);
+  });
+
+  it('should return 400 with invalid uniqueHash', (done) => {
+    request(app)
+      .post('/authenticate')
+      .set('Content-Type', 'application/json')
+      .send({ id: testUser.id, uniqueHash: testUser.uniqueHash + 'invalid' })
+      .expect(400, done);
+  });
+
+  it('should return 400 with invalid json', (done) => {
+    request(app)
+      .post('/authenticate')
+      .set('Content-Type', 'application/json')
+      .send("{ id: testUser.id, uniqueHash: testUser.uniqueHash + 'invalid' ")
+      .expect(400, done);
+  });
+
+  it('should return 200 with valid user', (done) => {
+    request(app)
+      .post('/authenticate')
+      .set('Content-Type', 'application/json')
+      .send(testUser)
+      .expect(200)
+      .end((err, res) => {
+        if (!err) {
+          expect(res.body.token).toBeTruthy();
+        }
+        return done();
+      });
   });
 });
 
 describe('GET /top', () => {
+  let token = '';
+  beforeAll(() => {
+    // create token for accessing secured content
+    req
+      .post('/authenticate')
+      .set('Content-Type', 'application/json')
+      .send(testUser)
+      .end((err, res) => {
+        if (!err) {
+          expect(res.body).toBeTruthy();
+          assert(res.body.token);
+          token = res.body.token;
+        }
+      });
+  });
+
   const req = request.agent(app);
-  it('shoult not be ratelimited', (done) => {
-    req.get('/top').expect(200, done);
+  it('shoud get 401 without token', (done) => {
+    req.get('/top').expect(401, done);
   });
-  it('should not be ratelimited', (done) => {
-    req.get('/top').expect(200, done);
+
+  it('should get 200 with token', (done) => {
+    if (token) {
+      req
+        .get('/top')
+        .set('Authorization', 'Bearer ' + token)
+        .expect(200, done);
+    } else {
+      fail('token not created');
+    }
   });
-  it('should not be ratelimited', (done) => {
-    req.get('/top').expect(200, done);
-  });
-  it('should not be ratelimited', (done) => {
-    req.get('/top').expect(200, done);
-  });
-  it('should "be ratelimited"', (done) => {
-    req.get('/top').expect(429, done);
-  });
+
+  // can not make 5 consequetive requests constantly,
+  // the number of requests to check here is 3, becase of the above
+  // two tests
+
+  for (let i = 0; i < 4; i++) {
+    let title = '';
+    let statusCode = 200;
+    if (i < 2) {
+      title = 'should not be rate limited';
+      statusCode = 200;
+    } else {
+      title = 'should be rate limited';
+      statusCode = 429;
+    }
+    it(title, (done) => {
+      if (token) {
+        req
+          .get('/top')
+          .set('Authorization', 'Bearer ' + token)
+          .expect(statusCode, done);
+      } else {
+        fail('token not created');
+      }
+    });
+  }
 });
